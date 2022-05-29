65182131a83d72969956bbcb13fbd02f7d157b90
Tay Yang Shun <tay.yang.shun@gmail.com>
2022-05-29 18:14:50 +0800

    feat: implement basic reporter

--- src/reporter.js
+++ src/reporter.js
@@ -1,19 +1,37 @@
 const INDENDATION_PER_LEVEL = 2;
-const resultsMap = {
+const RESULT_SYMBOLS = {
   true: '✔',
   false: '✖',
 };
 
-function printIndentedLog(level, message) {
-  console.log(
-    `${Array(level * INDENDATION_PER_LEVEL)
-      .fill(' ')
-      .join('')}${message}`,
-  );
+function formatIndentedLog(level, message) {
+  return `${Array(level * INDENDATION_PER_LEVEL)
+    .fill(' ')
+    .join('')}${message}`;
+}
+
+function visitor(node, level, logs) {
+  switch (node.type) {
+    case 'suite':
+      logs.push(formatIndentedLog(level, node.name));
+      node.children.forEach((childNode) => visitor(childNode, level + 1, logs));
+      break;
+
+    case 'spec':
+      logs.push(
+        formatIndentedLog(
+          level,
+          `${RESULT_SYMBOLS[node.passed]} ${node.name} (${node.duration}ms)`,
+        ),
+      );
+      break;
+  }
 }
 
 function reporter(results) {
-  console.log(JSON.stringify(results, null, 2));
+  const logs = [];
+  results.forEach((node) => visitor(node, 0, logs));
+  console.log(logs.join('\n'));
 }
 
 module.exports = reporter;
